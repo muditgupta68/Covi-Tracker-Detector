0ab865db5e219571ddb753e81c47de0f78d38cc9
muditgupta68 <muditgupta68@gmail.com>
2021-09-15 16:08:40 +0530

    app deployement exe file

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-        #     # self.Tracking_button.blockSignals(False)
-        #     self.Tracking_button.clicked.connect(self.trackstate)
-        # else:
-        #     self.Tracking_button.clicked.connect(self.trackCountry)
